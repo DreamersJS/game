26edb5340f0746151bc66a2e86f0597c5a2f6a66
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-04-07 14:48:48 +0300

    add: projectile

--- GONG/script.js
+++ GONG/script.js
@@ -9,11 +9,13 @@ class InputHandler{
     constructor(game){
         this.game = game;
         window.addEventListener('keydown', (event) => {
-            console.log(event);
-            console.log(this.game.keys);
+            // console.log(event);
+            // console.log(this.game.keys);
             if (((event.key === 'ArrowUp') || (event.key === 'ArrowDown')) && this.game.keys.indexOf(event.key) === -1){
                 this.game.keys.push(event.key);
-            }   
+            } else if(event.key === ' '){
+                this.game.player.shoot();
+            } 
         window.addEventListener('keyup', (event) => {
             if(this.game.keys.indexOf(event.key) > -1){
                 this.game.keys.splice(this.game.keys.indexOf(event.key), 1);
@@ -23,7 +25,27 @@ class InputHandler{
         });
     }
 }
-class Projecile{}
+class Projectile{
+    constructor(game,x,y){
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.width = 10;
+        this.height = 3;
+        this.speed = 3;
+        this.markedForDeletion = false;
+    }
+    update(){
+        this.x += this.speed;
+        if(this.x > this.game.width){
+            this.markedForDeletion = true;
+        }
+    }
+    draw(context){
+        context.fillStyle = 'grey';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
 class Particle{}
 class Player{
     constructor(game){
@@ -33,21 +55,36 @@ class Player{
         this.x = 20;
         this.y = 100;
         this.speedY = 0;
-        this.maxSpeed = 2;
+        this.maxSpeed = 3;
+        this.projectiles = [];
     }
     update(){
         if(this.game.keys.includes('ArrowUp')){
-            this.speedY = -1;
+            this.speedY =-this.maxSpeed;
         } else if(this.game.keys.includes('ArrowDown')){
-            this.speedY = 1;
+            this.speedY = this.maxSpeed;
         } else {
             this.speedY = 0;
         }
         this.y += this.speedY;
-    }
+        this.projectiles.forEach((projectile) => {
+            projectile.update();
+        });
+        this.projectiles = this.projectiles.filter((projectile) => !projectile.markedForDeletion);
+        }
 
     draw(context){
+        context.fillStyle = 'black';
         context.fillRect(this.x, this.y, this.width, this.height);
+        this.projectiles.forEach((projectile) => {
+            projectile.draw(context);
+        });
+    }
+
+    shoot(){
+        const projectile = new Projectile(this.game, this.x, this.y );
+        this.projectiles.push(projectile);
+        // console.log(this.projectiles);
     }
 }
 class Enemy{}
