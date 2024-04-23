1b6951c7dad410afc8703f9c8d6a96a1b145ace4
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-04-23 12:57:19 +0300

    add enemy

--- GONG/script.js
+++ GONG/script.js
@@ -87,23 +87,92 @@ class Player{
         // console.log(this.projectiles);
     }
 }
-class Enemy{}
+class Enemy{
+    constructor(game){
+        this.game = game;
+        this.x = this.game.width;
+        this.speedX = Math.random() * -1,5 - 0.5;
+        this.markedForDeletion = false;
+    }
+    draw(context){
+        context.fillStyle = 'red';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update(){
+        this.x += this.speedX;
+        if(this.x + this.width < 0){
+            this.markedForDeletion = true;
+        }
+    }
+}
+class Angler1 extends Enemy{
+    constructor(game){
+        super(game);
+        this.width = 228;
+        this.height = 169;
+        this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    }
+
+}
 class Layer{}
 class Background{}
-class UI{}
+// class UI{
+//     constructor(game){
+//         this.game = game;
+//         this.fontSize = 25;
+//         this.fontFamily = 'Helvetica';
+//         this.color = 'white';
+//     }
+// }
 class Game{
     constructor(width, height){
         this.width = width;
         this.height = height;
         this.player = new Player(this);
         this.input= new InputHandler(this);
+        // this.ui = new UI(this);
         this.keys = [];
+        this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemyInterval = 1000;
+        this.gameOver = false;
     }
     update(){
         this.player.update();
+        this.enemies.forEach((enemy) => {
+            enemy.update();
+        });
+        this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+        if (this.enemyTimer > this.enemyInterval && !this.gameOver){
+            this.addEnemy();
+            this.enemyTimer = 0;
+            
+            
+        }else{
+            this.enemyTimer += 2.3; // to be replaced with deltaTime
+        }
     }
     draw(context){
         this.player.draw(context);
+        // this.ui.draw(context);
+        this.enemies.forEach((enemy) => {
+            enemy.draw(context);
+        });
+    }
+
+    addEnemy(){
+        const enemy = new Angler1(this);
+        this.enemies.push(enemy);
+        // console.log(this.enemies);
+    }
+
+    checkCollisions(rect1, rect2){
+        return(
+            rect1.x < rect2.x + rect2.width &&
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.y + rect1.height > rect2.y
+        )
     }
 }
 
